97003447c3f8a19d9d2754de4ae3d9d4c8b61744
jose <joseantonio.pascual@ehu.es>
2014-07-01 09:45:55 +0200

    First commit

---
+++ src/infrastructure/provider.cc
@@ -0,0 +1,21 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  provider.cc
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  07/01/14 09:29:03
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Jose A. Pascual (), joseantonio.pascual@ehu.es
+ *   Organization:  University of the Basque Country UPV/EHU
+ *
+ * =====================================================================================
+ */
+
+
+
+
